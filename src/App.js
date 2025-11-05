c27a499b4cc70af9a54ad7637ab5f78af3d67b93
Ishan Lakhotia <Ishanlakhotia@gmail.com>
2025-11-05 18:04:11 -0500

    Started working on the main page of this project.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="stars"></div>
+      <div class="shooting-star"></div>
+      <div class="shooting-star"></div>
+      <div class="shooting-star"></div>
+      <div class="shooting-star"></div>
+      <div class="shooting-star"></div>
+      <div className="App"> 
+        <div className="App-header">
+        <h4>Welcome to the Detectify App. An ML image classification model!</h4>
+        <p>Upload your image, for image classification!</p>
+        
+        <form>
+          <div className='container'>
+          <label>Image Title: </label>
+          <input type='text' placeholder='Enter a title for the image' />
+          <label>Upload Image: </label>
+          <input type='file' name='img' accept='image/*' />
+          </div>
+        </form>
+        </div>
+      </div>
+    </>
   );
 }
 
